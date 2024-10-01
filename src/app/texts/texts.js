76901d8073a3bf21982038c952a53c386328e967
kevinpicon01 <digitalpruu@gmail.com>
2024-10-01 14:30:00 -0500

    Changed media, color, texts

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,9 +1,9 @@
-const title = "oscar ";
-const email = "Email del emprendimiento";
+const title = "Oscar Carreño";
+const email = "Pinillamenoscar@gmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    `¡Hola! Estoy interesado en los productos de ${title} y me gustaría recibir más información. ¡Espero su respuesta!`
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "3123506414";
 const textos = {
     index: {
         title: title,
@@ -12,25 +12,24 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "Oscar Carreño se especializa en bolsos para dama que combinan estilo y elegancia. Con nuestra variedad de diseños, cada bolso refleja el gusto que te define.",
+        description2: "Nuestros bolsos están creados con materiales de alta calidad y atención a los detalles, brindando una experiencia única para quienes valoran la moda y la funcionalidad. En *Oscar Carreño*, creemos que cada bolso es una expresión de personalidad y buen gusto.",
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
-        email: email
+        email: email,
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "Oscar Carreño",
+            p2: "El gusto que te define.",
         },
-        address: "Dirección del emprendimiento",
+        address: "",
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
-        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
+        facebook: "https://www.facebook.com/profile.php?id=61565365298341&mibextid=ZbWKwL",
+        instagram: "https://www.instagram.com",
+        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
     },
-
 };
 export default textos;
